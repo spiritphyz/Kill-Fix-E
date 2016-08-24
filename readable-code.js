3d871ce75f0e10e3cfc27ea0f77935df20f6de55
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-23 18:19:01 -0700

    human-readable form

---
+++ readable-code.js
@@ -0,0 +1,25 @@
+/** 
+ * Remove fixed elements that block page content.
+ * Remove no-scroll behavior.
+ */
+
+javascript:(
+  function() {
+    var scrollPreventers = ['signup_wall_prevent_scroll', 'no-scroll'];
+    var elements = document.querySelectorAll('body *');
+    for (var i = 0; i < elements.length; i += 1) {
+      if (window.getComputedStyle(elements[i]).position === 'fixed') {
+        elements[i].parentNode.removeChild(elements[i]);
+      }
+    }
+    var overlays = document.querySelectorAll('div[id*=signup_wall_wrapper]');
+    for (var i = 0; i < overlays.length; i += 1) {
+      overlays[i].parentNode.removeChild(overlays[i]);
+    }
+    scrollPreventers.forEach(function(className) {
+      if (document.body.classList.contains(className)) {
+        document.body.classList.remove(className);
+      }
+    });
+  }
+)();
